8e11bdc8135100536b9cc1109ed440be35670861
Patrik Dahlström <risca@dalakolonin.se>
2021-11-20 00:50:07 +0100

    Add support for bifferboard

---
+++ board/bifferos/common/post_image.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eux
+./board/bifferos/common/tools/mkimg_bifferboard.py "${BINARIES_DIR}/bzImage" "${BINARIES_DIR}/rootfs.squashfs" "${BINARIES_DIR}/flash.img" 0x10
+dd if=/dev/zero of="${BINARIES_DIR}/qemu.img" bs=1024 count=16
+./board/bifferos/common/tools/make_config_block.py 0x10 "${BINARIES_DIR}/config.bin"
+cat "${BINARIES_DIR}/config.bin" >> "${BINARIES_DIR}/qemu.img"
+cat "${BINARIES_DIR}/flash.img" >> "${BINARIES_DIR}/qemu.img"
